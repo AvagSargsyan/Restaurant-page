7f4e13ed83433a684861023b8c1b4c80fbef4401
Avag <avagsargsyan01@gmail.com>
2022-07-15 19:24:19 +0400

    Finish styling of menu page

--- src/menu.js
+++ src/menu.js
@@ -35,29 +35,41 @@ function createMenu(content) {
 
     const menuItem4 = document.createElement('div');
     const menuTitle4 = document.createElement('h2');
-    menuTitle4.innerText = 'SALADS';
+    menuTitle4.innerText = 'LAMB';
     const menuContent4 = document.createElement('p');
     menuItem4.setAttribute('id', 'menu-item1');
-    menuContent4.innerHTML = 'Tasty Meal .......... 4$ <br>Tasty Meal .......... 9$$ <br>Tasty Meal .......... 8,50$ <br>Tasty Meal .......... 13$ <br>Tasty Meal .......... 6,90$ <br>Tasty Meal .......... 3,50$ <br>Tasty Meal .......... 4$ <br>Tasty Meal .......... 8,50$ <br>Tasty Meal .......... 5$ <br>Tasty Meal .......... 7,70$ <br>'
+    menuContent4.innerHTML = 'Tasty Meal .......... 9$ <br>Tasty Meal .......... 19$$ <br>Tasty Meal .......... 15,50$ <br>Tasty Meal .......... 13$ <br>Tasty Meal .......... 20,90$ <br>Tasty Meal .......... 33,50$ <br>Tasty Meal .......... 10$ <br>Tasty Meal .......... 8,50$ <br>'
 
     menuItem4.appendChild(menuTitle4);
     menuItem4.appendChild(menuContent4);
 
     const menuItem5 = document.createElement('div');
     const menuTitle5 = document.createElement('h2');
-    menuTitle5.innerText = 'DRINKS';
+    menuTitle5.innerText = 'SALADS';
     const menuContent5 = document.createElement('p');
     menuItem5.setAttribute('id', 'menu-item1');
-    menuContent5.innerHTML = 'Good Drink .......... 10$ <br>Good Drink .......... 19$$ <br>Good Drink .......... 15,50$ <br>Good Drink .......... 13$ <br>Good Drink .......... 20,90$ <br>Good Drink .......... 33,50$ <br>Good Drink .......... 10$ <br>Good Drink .......... 8,50$ <br>'
+    menuContent5.innerHTML = 'Tasty Meal .......... 4$ <br>Tasty Meal .......... 9$$ <br>Tasty Meal .......... 8,50$ <br>Tasty Meal .......... 13$ <br>Tasty Meal .......... 6,90$ <br>Tasty Meal .......... 3,50$ <br>Tasty Meal .......... 4$ <br>Tasty Meal .......... 8,50$ <br>Tasty Meal .......... 5$ <br>Tasty Meal .......... 7,70$ <br>'
 
     menuItem5.appendChild(menuTitle5);
     menuItem5.appendChild(menuContent5);
+
+
+    const menuItem6 = document.createElement('div');
+    const menuTitle6 = document.createElement('h2');
+    menuTitle6.innerText = 'DRINKS';
+    const menuContent6 = document.createElement('p');
+    menuItem6.setAttribute('id', 'menu-item1');
+    menuContent6.innerHTML = 'Good Drink .......... 10$ <br>Good Drink .......... 19$$ <br>Good Drink .......... 15,50$ <br>Good Drink .......... 13$ <br>Good Drink .......... 20,90$ <br>Good Drink .......... 33,50$ <br>Good Drink .......... 10$ <br>Good Drink .......... 8,50$ <br>'
+
+    menuItem6.appendChild(menuTitle6);
+    menuItem6.appendChild(menuContent6);
 
     menuContainer.appendChild(menuItem1);
     menuContainer.appendChild(menuItem2);
     menuContainer.appendChild(menuItem3);
     menuContainer.appendChild(menuItem4);
     menuContainer.appendChild(menuItem5);
+    menuContainer.appendChild(menuItem6);
 
     return menuContainer;
 }
